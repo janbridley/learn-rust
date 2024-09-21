a09554dcb935c03135676e0e88091dc02f636ffc
janbridley <bradleyjenj@gmail.com>
2024-09-21 14:23:28 -0400

    Update through guessing game

---
+++ book-projects/guessing_game/src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn _enter_guess() -> String {
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // Conversion to i32 must be done in main to avoid continue outside loop
+    return guess;
+}
+
+fn main() {
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+
+    println!("The secret number is {secret_number}");
+
+    loop {
+
+        // parse returns a Result (enum with vars Ok and Err)
+        // match is like a switch (?) that returns num or continues
+        let guess: i32 = match _enter_guess().trim().parse() {
+            Ok(num) => num,
+            Err(_) => {println!("Invalid number!"); continue},
+        };
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {println!("You win!"); break;},
+        }
+    }
+
+}
